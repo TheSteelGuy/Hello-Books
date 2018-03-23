a70dae27c0124a1491063bf453457fd9b348f6e9
TheSteelGuy <collotuos0@gmail.com>
2018-03-23 22:09:32 +0300

    initial commit

---
+++ app/__init__.py
@@ -0,0 +1,25 @@
+#app/__init__.property
+#coding:utf-8
+
+"""the init file controls the application
+   all essential configaration are done here
+"""
+#third party imports
+from flask import Flask
+#local imports
+from app.config import app_config
+from  classes import User
+from classes import Admin
+
+user = User()
+admin_user = Admin()
+
+def create_app(config_name):
+    app = Flask(__name__)
+    app.config.from_object(app_config[config_name])
+    from admin import admin as admin_blueprint
+    from home import home as home_blueprint
+    app.register_blueprint(admin_blueprint)
+    app.register_blueprint(home_blueprint)
+
+    return app
