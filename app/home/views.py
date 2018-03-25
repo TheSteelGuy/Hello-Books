0d2195dffd2ec7a800542133b8c1d578adb06df2
TheSteelGuy <collotuos0@gmail.com>
2018-03-25 11:56:36 +0300

    [feature] add login view

--- app/home/views.py
+++ app/home/views.py
@@ -2,7 +2,7 @@
 
 from flask import make_response,request,jsonify
 #local imports
-from app import user
+from app import user,admin_user
 from . import home
 
 @home.route('/')
@@ -24,28 +24,69 @@ def register():
             }
         )), 200
     if request.method == 'POST':
-        details = request.get_json()
-        username = details.get('username')
-        email = details.get('email')
-        password = details.get('password')
-        confirm_pwd = details.get('confirm_pwd')
-        try:
-            response = user.register(username,email,password,confirm_pwd)
-            return make_response(jsonify(
-                {'message':response}
-            )) 
-        except Exception as e:
-            return make_response(jsonify(
-                {'message':e}
-            )), 401
-    return make_response(jsonify(
-        {'message':'method not allowed,sorry'}
-    )), 405
+        username = request.form.get('username')
+        email = request.form.get('email')
+        password = request.form.get('password')
+        confirm_pwd = request.form.get('confirm_pwd') 
+        response = user.register(username,email,password,confirm_pwd)
+        if response == 'username exists choose another name!':    
+           return make_response(jsonify(
+               {'message':'username exists choose another name!'}
+           )), 400
+        if response == "Email is already in use":    
+           return make_response(jsonify(
+               {'message':"Email is already in use"}
+           )), 400
+        if response == "Invalid email":    
+           return make_response(jsonify(
+               {'message':response}
+           )), 400
+        if response == "password length should be more than 3 characters":    
+           return make_response(jsonify(
+               {'message':response}
+           )), 400
+        if response == "username cannot be empty, or non alphabet":    
+           return make_response(jsonify(
+               {'message':response}
+           )), 400
+        if response == "password does not match":    
+           return make_response(jsonify(
+               {'message':response}
+           )), 400
+        if response == "registration succesfull":    
+           return make_response(jsonify(
+               {'message':response}
+           )), 201
 
+  
 @home.route('/auth/api/v1/login',methods=['GET','POST'])
 def login():
     """handles login requests"""
     if request.method == 'GET':
         return make_response(jsonify(
             {'message':'welcome to the login endpoint'}
+        )), 200
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        response = user.login(email,password)
+        if response == "succsefully logged in":
+            return make_response(jsonify(
+                {'message':response}
+            )), 200
+        if response == "Inavlid username password":
+            return make_response(jsonify(
+                {'message':response}
+            )), 400
+        if response == "you have no account, register":
+            return make_response(jsonify(
+                {'message':response}
+            )), 400
+            
+@home.route('/api/v1/books')
+def get_books():
+    if request.method == 'GET':
+        res = admin_user.books_list
+        return make_response(jsonify(
+            {'books':res}
         )), 200
