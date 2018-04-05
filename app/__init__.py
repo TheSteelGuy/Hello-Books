ac23e7fc9cfcd02317f3e9a1b3d3d5077a347156
TheSteelGuy <collotuos0@gmail.com>
2018-04-05 19:21:05 +0300

    [fix] add rule to allow appended slashes in url

--- app/__init__.py
+++ app/__init__.py
@@ -11,23 +11,24 @@ from datetime import timedelta
 import os
 #local imports
 from app.config import app_config
-from  .models.user import User
+from  .models.user import Users
 from .models.admin import Admin
 from . error import method_not_allowed, server_error, resource_not_found
 
-user_object = User()
+user_object = Users()
 admin_user = Admin()
 
 def create_app(config_name):
     app = Flask(__name__)
     app.config['PRESERVE_CONTEXT_ON_EXCEPTION'] = False
-    app.config['JWT_SECRET_KEY'] = os.urandom(10) 
+    app.url_map.strict_slashes = False
+    app.config['JWT_SECRET_KEY'] = os.urandom(24) 
     app.config.from_object(app_config[config_name])
     ACCESS_EXPIRES = timedelta(minutes=15)
     REFRESH_EXPIRES = timedelta(days=10)
     app.config['JWT_ACCESS_TOKEN_EXPIRES'] = ACCESS_EXPIRES
     app.config['JWT_REFRESH_TOKEN_EXPIRES'] = REFRESH_EXPIRES
-    jwt = JWTManager(app)
+    JWTManager(app)
     from .admin import admin as admin_blueprint
     from .user import user as user_blueprint
     app.register_blueprint(admin_blueprint)
